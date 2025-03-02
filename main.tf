3bf9b0506fa1fef1b96c32cb106fbbc8582b2bc4
unknown <nikitakumari503@gmail.com>
2025-03-02 19:40:24 +0530

    terraform format fix

--- main.tf
+++ main.tf
@@ -1,105 +1,105 @@
 provider "google" {
-    project = "vcc-25"
-    region  = "us-central1"
-    zone    = "us-central1-a"
+  project = "vcc-25"
+  region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
 resource "google_compute_instance_template" "vcc" {
-    name         = "vm-vcc"
-    machine_type = "n1-standard-1"
+  name         = "vm-vcc"
+  machine_type = "n1-standard-1"
 
-    disk {
-        source_image = "ubuntu-os-cloud/ubuntu-2004-lts"
-        auto_delete  = true
-        boot         = true
-    }
-    network_interface {
-        network = "default"
-        access_config {}
-    }
-    metadata = {
-        startup-script = <<-EOF
+  disk {
+    source_image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    auto_delete  = true
+    boot         = true
+  }
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+  metadata = {
+    startup-script = <<-EOF
             #!/bin/bash
             apt update && apt install -y apache2
             echo "Hello, Terraform VM!" > /var/www/html/index.html
             systemctl restart apache2
         EOF
-    }
-    tags = ["http-server"]
+  }
+  tags = ["http-server"]
 }
 
 resource "google_compute_instance_group_manager" "vcc" {
-    name               = "igm-vcc"
-    base_instance_name = "vm-vcc"
-    zone               = "us-central1-a"
+  name               = "igm-vcc"
+  base_instance_name = "vm-vcc"
+  zone               = "us-central1-a"
 
-    version {
-        instance_template = google_compute_instance_template.vcc.id
-        name              = "primary"
-    }
-    target_size = 3
+  version {
+    instance_template = google_compute_instance_template.vcc.id
+    name              = "primary"
+  }
+  target_size = 3
 
-    auto_healing_policies {
-        health_check      = google_compute_health_check.default.id
-        initial_delay_sec = 300
-    }
+  auto_healing_policies {
+    health_check      = google_compute_health_check.default.id
+    initial_delay_sec = 300
+  }
 }
 
 resource "google_compute_autoscaler" "vcc" {
-    name   = "autoscaler-for-vm"
-    zone   = "us-central1-a"
-    target = google_compute_instance_group_manager.vcc.id
+  name   = "autoscaler-for-vm"
+  zone   = "us-central1-a"
+  target = google_compute_instance_group_manager.vcc.id
 
-    autoscaling_policy {
-        max_replicas    = 5
-        min_replicas    = 1
-        cooldown_period = 60
+  autoscaling_policy {
+    max_replicas    = 5
+    min_replicas    = 1
+    cooldown_period = 60
 
-        cpu_utilization {
-        target = 0.7
-        }
+    cpu_utilization {
+      target = 0.7
     }
+  }
 }
 
 resource "google_compute_health_check" "default" {
-    name                = "health-check"
-    timeout_sec         = 5
-    check_interval_sec  = 30
-    http_health_check {
-        port = 80
-    }
+  name               = "health-check"
+  timeout_sec        = 5
+  check_interval_sec = 30
+  http_health_check {
+    port = 80
+  }
 }
 
 resource "google_project_iam_custom_role" "vm_admin" {
-    role_id     = "vmAdmin"
-    title       = "VM Admin"
-    description = "Custom role for VM administration"
-    permissions = [
-        "compute.instances.get",
-        "compute.instances.list",
-        "compute.instances.create",
-        "compute.instances.delete"
-    ]
+  role_id     = "vmAdmin"
+  title       = "VM Admin"
+  description = "Custom role for VM administration"
+  permissions = [
+    "compute.instances.get",
+    "compute.instances.list",
+    "compute.instances.create",
+    "compute.instances.delete"
+  ]
 }
 
 resource "google_compute_firewall" "allow_http" {
-    name    = "allow-http"
-    network = "default"
+  name    = "allow-http"
+  network = "default"
 
-    allow {
-        protocol = "tcp"
-        ports    = ["80"]
-    }
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
 
-    source_ranges = ["0.0.0.0/0"]
-    target_tags   = ["http-server"]
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
 }
 
 resource "google_project_iam_binding" "vm_admin_binding" {
-    project = "vcc-25"
-    role    = google_project_iam_custom_role.vm_admin.id
+  project = "vcc-25"
+  role    = google_project_iam_custom_role.vm_admin.id
 
-    members = [
-        "serviceAccount:vcc-10@vcc-25.iam.gserviceaccount.com"
-    ]
+  members = [
+    "serviceAccount:vcc-10@vcc-25.iam.gserviceaccount.com"
+  ]
 }
